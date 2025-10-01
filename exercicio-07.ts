b682dec221d83d21c2173137569d8a32a2ddfa55
Felipem552 <felipemendes5552@gmail.com>
2025-10-01 10:47:53 -0300

    Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.

---
+++ exercicio-07.ts
@@ -0,0 +1,12 @@
+// Exercício 7: Declare três variáveis e atribua valores numéricos a elas. 
+// Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+let a: number = 10
+let b: string = '10' 
+let c: number = 5
+
+console.log(a == b)      
+console.log(a === b) //O valor dos dois são iguais a 10. Porém um é Number, e o outro é String. Por isso não são estritamente iguais    
+console.log(a != b)       
+console.log(a > c)       
+console.log(c <= a)       
