0333d1120629d3637e23931214baaf73d15c8212
Felipem552 <felipemendes5552@gmail.com>
2025-10-01 10:42:50 -0300

    Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.

---
+++ exercicio-05.ts
@@ -0,0 +1,7 @@
+//Exercício 05: Declare duas variáveis e atribua valores numéricos a elas. Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+let a: number = 4
+let b: number = 6
+let soma: number = (a+b)
+
+console.log(soma)
