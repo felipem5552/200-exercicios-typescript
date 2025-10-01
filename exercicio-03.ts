3126b8f90ffe3d6a10b9c5ca40b7ab92a72f9235
Felipem552 <felipemendes5552@gmail.com>
2025-10-01 10:12:05 -0300

    Declare uma variável chamada númeroMáximo, atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.

---
+++ exercicio-03.ts
@@ -0,0 +1,12 @@
+//Exercício 03: Declare uma variável chamada "númeroMáximo", 
+//atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.
+
+const numeroMaximo: number = Number.MAX_SAFE_INTEGER
+
+alert (`Curiosidade do Dia!
+
+Você sabia que o maior valor inteiro seguro no JavaScript/Typescript é utilizando o Number.MAX_SAFE_INTEGER ?
+
+Ao rodar esta constante você obterá o seguinte valor: 
+                                
+                            ${numeroMaximo}`)
