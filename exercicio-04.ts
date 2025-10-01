fbf54d1385f5e2642fee152079d84d98b39e070c
Felipem552 <felipemendes5552@gmail.com>
2025-10-01 10:33:35 -0300

    Declare duas variáveis chamadas nome e sobrenome, atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada nomeCompleto e imprima.

---
+++ exercicio-04.ts
@@ -0,0 +1,10 @@
+// Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome",
+// atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome: string = "Felipe"
+let sobrenome: string = "Mendes Avves"
+
+let nomeCompleto: string = nome + " " + sobrenome
+
+
+console.log(`👋 Olá, ${nomeCompleto}!`)
