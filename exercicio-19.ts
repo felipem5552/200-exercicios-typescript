1340c09f149aac2374fd56895be7b4d4622f398b
unknown <felipemendes5552@gmail.com>
2025-10-08 22:30:12 -0300

    Verifique se um número está dentro de um intervalo.

---
+++ exercicio-19.ts
@@ -0,0 +1,9 @@
+//Exercício 19: Verifique se um número está dentro de um intervalo.
+
+let n: number = 15
+
+if (n >= 10 && n <= 20) {
+  console.log("Número dentro do intervalo")
+} else {
+  console.log("Número fora do intervalo")
+}
