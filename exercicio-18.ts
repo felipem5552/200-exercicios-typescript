c4729c0c0ca46b0ea8ffd2dc3d4dfc83ced15d48
unknown <felipemendes5552@gmail.com>
2025-10-08 22:21:43 -0300

    Verifique se uma pessoa pode dirigir com base na idade.

---
+++ exercicio-18.ts
@@ -0,0 +1,13 @@
+//Exercício 18: Verifique se uma pessoa pode dirigir com base na idade.
+
+let idade: number = 19
+
+
+if (idade >= 18) {
+    console.log(`Parabéns! Com ${idade} anos você pode dirigir`)
+} 
+else {
+   console.log(`Com ${idade} anos você NÃO pode dirigir`)
+}
+
+//console.log("Parabéns! Com 19 anos você pode dirigir")
