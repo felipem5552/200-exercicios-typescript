bfd1e28e53ac610a4d4bd60df24f14a1e8aa8fca
unknown <felipemendes5552@gmail.com>
2025-10-08 22:17:35 -0300

    Escreva um programa que determina o maior entre três números.

---
+++ exercicio-16.ts
@@ -0,0 +1,19 @@
+//Exercício 16: Escreva um programa que determina o maior entre três números.
+
+let a: number = 5
+
+let b: number = 15
+
+let c: number = 12
+
+if (a > b && a > c) {
+    console.log(`O maior número é ${a}`)
+}
+
+if (b > a && b > c) {
+    console.log(`O maior número é ${b}`)
+}
+
+if (c > a && c > b) {
+    console.log(`O maior número é ${c}`)
+}
