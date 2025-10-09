59274fcedcd40529a8c9d0577a0cca730e9c3e27
unknown <felipemendes5552@gmail.com>
2025-10-08 23:15:21 -0300

    Imprima uma classificação baseada na nota de um aluno.

---
+++ exercicio-20.ts
@@ -0,0 +1,15 @@
+//Exercício 20: Imprima uma classificação baseada na nota de um aluno.
+
+let nota: number = 9
+
+if( nota >= 7) {
+  console.log("Parabéns, você foi aprovado!")
+}
+else if( nota >= 5) {
+  console.log("Você está de recuperação, continue se esforçando!")
+}
+else {
+  console.log("Infelizmente você foi reprovado!")
+}
+
+//console.log("Parabéns, você foi aprovado!")
