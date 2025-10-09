7e73c97245d65c3548558b07c54d53af8f60e7f8
unknown <felipemendes5552@gmail.com>
2025-10-08 22:19:30 -0300

    Escreva um programa que determina se um número é par ou ímpar.

---
+++ exercicio-17.ts
@@ -0,0 +1,10 @@
+//Exercício 17: Escreva um programa que determina se um número é par ou ímpar.
+
+let num: number = 4
+
+if (num % 2 === 0) {
+    console.log(`O número ${num} é par.`)
+}
+else console.log(`O número ${num} é ímpar.`)
+
+//console.log("O número 4 é par")
