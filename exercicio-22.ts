91034dcb097da100fbef801fdca4bb8053c70f47
unknown <felipemendes5552@gmail.com>
2025-10-14 22:14:51 -0300

    Escreva um programa que determina o número de dias em um mês.

---
+++ exercicio-22.ts
@@ -0,0 +1,61 @@
+// Exercício 22: Escreva um programa que determina o número de dias em um mês.
+
+import * as readline from 'readline'
+
+// 1. Configura a interface de leitura/escrita.
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const menu = `Escolha um mês para verificar o número de dias que ele possui:
+1. Janeiro
+2. Fevereiro
+3. Março
+4. Abril
+5. Maio
+6. Junho
+7. Julho
+8. Agosto
+9. Setembro
+10. Outubro
+11. Novembro 
+12. Dezembro
+
+Digite o número referente à opção escolhida: `
+
+// 2. Faz a pergunta e processa a resposta.
+rl.question(menu, (mes: string) => {
+
+    let dias: string
+
+    switch (mes) {
+        case "1":
+        case "3":
+        case "5":
+        case "7":
+        case "8":
+        case "10":2
+        case "12":
+            dias = "O mês selecionado possui 31 dias."
+            break
+
+        case "4":
+        case "6":
+        case "9":
+        case "11":
+            dias = "O mês selecionado possui 30 dias."
+            break
+
+        case "2":   
+            dias = "O mês selecionado possui 28 dias, ou 29 dias em anos bissextos."
+            break
+
+        default:
+            dias = "Opção não identificada. Certifique-se de digitar um número de 1 a 12."
+    }
+
+    // 3. Exibe o resultado e fecha a interface.
+    console.log(dias)
+    rl.close()
+})
